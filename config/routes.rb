e9cbefe8414912eca0a740de7ff5168587f00138
Karina Miernik <karina.miernik@gmail.com>
2021-02-24 14:56:40 +0100

    form created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 Rails.application.routes.draw do
-  # GET /about
-  get "about-us", to: "about#index", as: 'about'
+
+  get "about", to: "about#index"
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
+
   root to: "main#index"
 end
