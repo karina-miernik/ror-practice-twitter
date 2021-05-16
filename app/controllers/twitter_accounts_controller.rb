9b3a5f20fc8a1e1484977fb576c755f67f0501b0
Karina Miernik <karina.miernik@gmail.com>
2021-05-16 10:51:35 +0200

    twitter account added

--- app/controllers/twitter_accounts_controller.rb
+++ app/controllers/twitter_accounts_controller.rb
@@ -1,5 +1,7 @@
 class TwitterAccountsController < ApplicationController
   before_action :require_user_logged_in!
+  before_action :set_twitter_account, only:[:destroy]
+
 
   def index
     @twitter_accounts = Current.user.twitter_accounts
@@ -9,5 +11,12 @@ class TwitterAccountsController < ApplicationController
     @twitter_account = Current.user.twitter_accounts.find(params[:id])
     @twitter_account.destroy
     redirect_to twitter_accounts_path, notice: "Successfully disconnected @#{@twitter_account.username}"
+  end
+  
+  private
+  
+  def set_twitter_account
+    @twitter_account = Current.user.twitter_accounts.find(params[:id])
+
   end
 end
--- db/migrate/20210324084710_create_twitter_accounts.rb
+++ db/migrate/20210324084710_create_twitter_accounts.rb
@@ -2,11 +2,11 @@ class CreateTwitterAccounts < ActiveRecord::Migration[6.1]
   def change
     create_table :twitter_accounts do |t|
       t.belongs_to :user, null: false, foreign_key: true
-      t.string :name
-      t.string :username
-      t.string :image
-      t.string :token
-      t.string :secret
+      t.text :name
+      t.text :username
+      t.text :image
+      t.text :token
+      t.text :secret
 
       t.timestamps
     end
