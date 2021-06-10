d068cc8ce10f114d9ea18fb40352629d3b801917
Karina Miernik <karina.miernik@gmail.com>
2021-06-10 10:10:11 +0200

    fixed typos

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
   before_action :require_user_logged_in!
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+
   def index
     @tweets = Current.user.tweets
   end
@@ -11,7 +12,7 @@ class TweetsController < ApplicationController
 
   def create
     @tweet = Current.user.tweets.new(tweet_params)
-    if @tweet.
+    if @tweet.save
       redirect_to tweets_path, notice: "Tweet was scheduled successfully"
     else
       render :new
@@ -23,14 +24,15 @@ class TweetsController < ApplicationController
 
   def update
     if @tweet.update(tweet_params)
-      redirect_to tweets_path, notice: "Update was successfull"
+      redirect_to tweets_path, notice: "Tweet was updated successfully"
+    else
+      render :edit
     end
   end
 
   def destroy
     @tweet.destroy
     redirect_to tweets_path, notice: "Tweet was unscheduled"
-
   end
 
   private
